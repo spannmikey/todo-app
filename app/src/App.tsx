346973f38940b8adb0969b7371c9a78b3fc7c3dd
mikey <spannmikey@gmail.com>
2023-11-10 14:49:17 -0600

    Getting value of user input in SearchBar component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react"
 import SearchBar from "./components/SearchBar"
 
 export default function App(): React.ReactNode {
+	const [userSearchInput, setUserSearchInput] = useState<string>("")
+
 	return (
 		<div className="App">
 			<h1 className="App__heading">Todo App</h1>
-			<SearchBar />
+			<SearchBar
+				userSearchInput={userSearchInput}
+				setUserSearchInput={setUserSearchInput}
+			/>
 		</div>
 	)
 }
--- app/src/components/SearchBar.tsx
+++ app/src/components/SearchBar.tsx
@@ -1,4 +1,13 @@
-export default function SearchBar() {
+interface Props {
+	userSearchInput: string
+	setUserSearchInput: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function SearchBar({
+	userSearchInput,
+	setUserSearchInput,
+}: Props) {
+	console.log(userSearchInput)
 	return (
 		<form
 			action="#"
@@ -6,6 +15,8 @@ export default function SearchBar() {
 			<input
 				className="SearchBar__input"
 				type="text"
+				value={userSearchInput}
+				onChange={e => setUserSearchInput(e.target.value)}
 				placeholder="Enter a todo item..."
 			/>
 			<button
